c49d0dd64753222c06091f41a922d78863cc5211
Unknown <nico@roechter.org>
2019-06-17 16:11:53 +0200

    Finish

--- MergePEMFiles/Form1.Designer.cs
+++ MergePEMFiles/Form1.Designer.cs
@@ -23,12 +23,32 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent () {
-            this.components = new System.ComponentModel.Container();
+            this.rtbxLog = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // rtbxLog
+            // 
+            this.rtbxLog.Location = new System.Drawing.Point(13, 13);
+            this.rtbxLog.Name = "rtbxLog";
+            this.rtbxLog.Size = new System.Drawing.Size(259, 236);
+            this.rtbxLog.TabIndex = 0;
+            this.rtbxLog.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.rtbxLog);
+            this.Name = "Form1";
+            this.Text = "Merge PEM Files";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox rtbxLog;
     }
 }
 
